037f438805e6b0dcd86a138b8b92757c9519faef
areggev <acr644@stern.nyu.edu>
2021-05-27 10:00:21 -0400

    Update game.py

--- game.py
+++ game.py
@@ -30,4 +30,12 @@ valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("COMPUTER CHOICE: ", computer_choice)
 
-print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN.")
+if(user_choice == "rock") and (computer_choice == "paper") or (user_choice == "paper") and (computer_choice == "rock") or (user_choice == "scissors") and (computer_choice == "paper"):
+    print("Congratulations, you won!")
+else:
+    if(user_choice == "rock") and (computer_choice == "scissors") or (user_choice =="paper") and (computer_choice == "scissors") or (user_choice == "scissors") and (computer_choice == "rock")
+        print("Sorry, you lost, please play again.")
+    else:
+        print(You tied, please play again.)
+
+print("THANKS FOR PLAYING ROCKS, PAPER, SCISSORS. PLEASE PLAY AGAIN.")
